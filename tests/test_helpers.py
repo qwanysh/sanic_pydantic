5b5b98638ae0e7bb17c38d28cbb1a9cc48d06ffe
qwanysh <olimpsdu@gmail.com>
2021-11-21 13:19:35 +0600

    add get_error_with_updated_location tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,9 @@
 import pytest
+from pydantic import BaseModel, ValidationError
+from pydantic.error_wrappers import ErrorWrapper
 
-from sanic_pydantic.helpers import serialize_query
+from sanic_pydantic.helpers import (get_error_with_updated_location,
+                                    serialize_query)
 
 
 @pytest.mark.parametrize('query, serialized_query', [
@@ -11,3 +14,10 @@ from sanic_pydantic.helpers import serialize_query
 ])
 def test_serialize_query(query, serialized_query):
     assert serialize_query(query) == serialized_query
+
+
+def test_get_error_with_updated_location():
+    errors = ErrorWrapper(Exception(), loc=())
+    error = ValidationError([errors], BaseModel)
+    updated_error = get_error_with_updated_location(error, 'new_location')
+    assert updated_error.raw_errors[0].loc_tuple() == ('new_location',)
